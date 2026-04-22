b751380a1a9ff0844affe3e789abadbc81e4363b
Blank-Truth <v01ds4v4nt001@gmail.com>
2026-04-22 20:39:01 +0100

    All Bugs Fixed Plus User Input Added

--- webscraper.mjs
+++ webscraper.mjs
@@ -2,10 +2,42 @@ import puppeteer from "puppeteer";
 import path from "path";
 import fs from  "fs";
 
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+const rl = readline.createInterface({ input, output });
+
 // This is a custom function that literally just tells Node to pause
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 async function scrapeMangaFreak() {
+
+    // String Formatting Logic
+    let mangaName = await rl.question("Enter manga name: ")
+    let mangaChapter = await rl.question("Enter chapter number: ")
+    rl.close();
+
+    function searchFormatting(title) {
+        let position = title.indexOf(" ")
+        // console.log(position)
+        if (position !== -1) { // Apparently if .indexOf() cannot find a space it returns -1
+            let formatted = title.slice(0, position)
+            return formatted.toLowerCase()
+        } else {
+            return title.toLowerCase()
+        }
+    }
+
+    function nameFormatting(mangaName, mangaChapter) {
+        let snaking = mangaName.replaceAll(" ", "_")
+        return `${snaking}_${mangaChapter}`
+    }
+
+    function numFormatting(name, chapter) {
+        let total = 36 + name.length + chapter.length + 1
+        return total
+    }
+
     // Launch browser 
     const browser = await puppeteer.launch({headless: false})
     const page = await browser.newPage()
@@ -15,7 +47,8 @@ async function scrapeMangaFreak() {
         waitUntil: "domcontentloaded",
         timeout: 60000
     });
-    const Manga = "Jujutsu Kaisen"
+
+    const Manga = mangaName
     await page.waitForSelector("input[name='search']")
     await page.type("input[name='search']", Manga, {delay: 100})
     await page.keyboard.press("Enter")
@@ -28,7 +61,7 @@ async function scrapeMangaFreak() {
     
     console.log("Status:", response.status());
     
-     // Download Images
+     // Download Cover Image
     async function downloadImage(url, filename) {
         const reply = await fetch(url)
         const buffer = await reply.arrayBuffer()
@@ -36,16 +69,16 @@ async function scrapeMangaFreak() {
         fs.writeFileSync(filename, Buffer.from(buffer))
     }
     
-    const imageUrl = await page.evaluate(() => {
-        const img = document.querySelector('img[src*="kaisen"]')
+    const imageUrl = await page.evaluate((searchSlice) => {
+        const img = document.querySelector(`img[src*=${searchSlice}]`)
         return img ? img.src : null;
-    })
-    console.log(imageUrl)
+    }, searchFormatting(mangaName))
+    // console.log(imageUrl)
     
     
     await downloadImage(imageUrl, "animePic.jpg")
 
-    await page.click('.manga_series_list a[href="/Read1_Jujutsu_Kaisen_238"]')
+    await page.click(`.manga_series_list a[href="/Read1_${nameFormatting(mangaName, mangaChapter)}"]`)
     await page.waitForSelector('.slider.round')
     
     await page.click('.slider.round')
@@ -56,30 +89,33 @@ async function scrapeMangaFreak() {
         return elements.map(element => element.src)
     })
     console.log(jpg)
-    console.log(jpg[1])
-    console.log(jpg[1].length)
+    console.log(`No. of chapters: ${jpg.length}`)
     
-    // Download JPEGS
+
+    // Download Images
     
     async function downloadPngs(filename) {
         const folderName = "Manga_Folder"
         fs.mkdirSync(folderName, {recursive: true})
-        for (let i=0; i<=filename.length; i++) {
-            const links = await fetch(filename[i])
-            // console.log(links)
-            const fullPath = path.join(folderName, filename[i].slice(69))
-            console.log(fullPath)
-            const buffer = await links.arrayBuffer()
-            // console.log(buffer)
-            fs.writeFileSync(fullPath, Buffer.from(buffer))
-            await delay(3000) // delay for 3000 milliseconds = 3 seconds
+        for (let i=0; i<(filename.length+1); i++) {
+            if (i<filename.length) {
+                const links = await fetch(filename[i])
+                // console.log(links)
+                const fullPath = path.join(folderName, filename[i].slice(numFormatting(mangaName, nameFormatting(mangaName, mangaChapter))))
+                console.log(fullPath)
+                const buffer = await links.arrayBuffer()
+                // console.log(buffer)
+                fs.writeFileSync(fullPath, Buffer.from(buffer))
+                await delay(3000) // delay for 3000 milliseconds = 3 seconds
+            } else {
+                await browser.close();
+            }
         }
     
     }
     downloadPngs(jpg)
 
 
-    // await browser.close();
 }
 
 
